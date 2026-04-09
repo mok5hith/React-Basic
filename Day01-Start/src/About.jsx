195e76e1f19bfb54d5a80b1417a22747d58e4ddb
Mokshith <mokshithnaikmoksh@gmail.com>
2026-04-10 00:01:09 +0530

    react-basic

---
+++ Day01-Start/src/About.jsx
@@ -0,0 +1,13 @@
+import App from './App'
+
+const About = () => {
+  return (
+    <div>
+      <h1> I'm in the About World <br />
+      hiii heliooo yeap  
+      </h1>
+    </div>
+  )
+}
+
+export default About;
---
+++ Day01-Start/src/App.jsx
@@ -0,0 +1,12 @@
+import About from './About'
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <About />
+    </div>
+  )
+}
+
+export default App
